0506e52c920dd3807b429d758ce788f0c0478355
vera <verapoles1@mail.ru>
2020-12-20 13:13:27 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+﻿$(document).ready(function() {
+
+$(".mobile__logo").on('click', function(){
+  if ($(".mobile__nav").hasClass("active")) {
+  	$(".mobile__nav").removeClass("active");
+  } else {
+  	$(".mobile__nav").addClass("active");
+  }
+});
+
+ 
+
+    var winWidth = $(window).width();
+
+    if (winWidth <= 1024) {
+   $('.slider-nav').slick('unslick');
+} else {
+   $('.slider-for').slick({infinite: true,
+  	        slidesToShow: 1,
+  	        slidesToScroll: 1,
+  	        arrows: false,
+  	        fade: true,
+  	        asNavFor: '.slider-nav'
+  	    });
+
+    $('.slider-nav').slick({
+    	infinite: true,
+    	slidesToShow: 7,
+    	slidesToScroll: 1,
+    	asNavFor: '.slider-for',
+    	dots: false,
+    	arrows: true,
+    	centerMode: true,
+    	focusOnSelect: true   	
+    });
+}
+
+});     
+
+    
